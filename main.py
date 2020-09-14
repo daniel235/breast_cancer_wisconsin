e6ee8bf3f0dc9326bca5264ed6e98ed091c5b777
daniel235 <daduncan298@gmail.com>
2020-09-14 01:58:31 -0500

    update

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from __future__ import absolute_import, division, print_function
 import pandas as pd 
+import tensorflow as tf 
+import numpy as np
+
 
 bc_data = pd.read_csv("data/breast_cancer_wisconsin.csv")
 print(bc_data)
+
