a89b000daba396052fc99d557f367a85bb6d2c0d
daniel235 <daduncan298@gmail.com>
2020-09-15 18:20:58 -0500

    neural net

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
 from __future__ import absolute_import, division, print_function
+from sklearn.linear_model import LogisticRegression
 import pandas as pd 
 import tensorflow as tf 
 import numpy as np
 
 
 bc_data = pd.read_csv("data/breast_cancer_wisconsin.csv")
-print(bc_data)
+
+y = bc_data["diagnosis"]
+bc_data = bc_data.drop("diagnosis", axis=1)
+bc_data = bc_data.drop(bc_data.columns[-1], axis=1)
+
+#train
+ytrain = y[:int(len(y) * .7)]
+bc_data_train = bc_data[:int(len(bc_data) * .7)]
+
+#test 
+ytest = y[int(len(y) * .7):]
+bc_data_test = bc_data[int(len(bc_data) * .7):]
+
+
+#create model
+model = LogisticRegression().fit(bc_data_train, ytrain)
+
+
+predictions = model.predict(bc_data_test)
+
+print(predictions)
+print(ytest)
+
+#neural network
+nn_model = tf.keras.Sequential([
+	#input layer
+	tf.keras.layers.Dense(len(bc_data_train.iloc[0]))
+	#hidden layer
+	tf.keras.layers.Dense(10, activation='relu')
+	
+])
+
+
+
+
 
